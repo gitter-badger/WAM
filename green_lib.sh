2bf2d3ec69d96c3b56198c92da8db43cd73e99c7
John Matthew Narofsky <7ytd765789@gmail.com>
2018-03-21 09:25:01 -0500

    Forgot to include my personal shell library. Fixed.

---
+++ green_lib.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+replace_newlines(){
+	sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/$2/g' "$1"
+}
+get_line_at(){
+	sed "$1q;d"
+}
+#substring(){
+#	printf "$1" | cut -c "$2-$3"
+#}
+str_length(){
+	printf "$1" | wc -m
+}
+substring(){
+	if [ $(echo "$3" | cut -c "-1") = "-" ] ; then
+		end=$( expr $(str_length "$1") + "$3" )
+	else
+		end="$3"
+	fi
+	#substring "$1" "$2" "$end"
+	printf "$1" | cut -c "$2-$end"
+}
